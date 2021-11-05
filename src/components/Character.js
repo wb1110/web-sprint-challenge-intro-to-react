9c5fc0fea7a2b8ea1b3f2cda6f08ceece9d383ca
wb1110 <strengthcoachwb@gmail.com>
2021-11-05 08:38:45 -0500

    connected files and passed props

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+    const character = props;
+    
+    return (
+        <h1>
+            'Hellow World';
+        </h1>
+    );
+};
+
+export default Character;
